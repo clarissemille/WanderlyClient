886ab5084b95face61647633536cb64adad161bf
clarissemille <cmille749@gmail.com>
2025-01-15 18:20:43 +0100

    setup

---
+++ src/pages/Wanderly.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Wanderly = () => {
+    return (
+        <div className="bg-homepage h-screen bg-cover bg-center">
+            <div className="text-center text-white relative top-1/2">
+                <h1 className="text-4xl font-bold">Wanderly</h1>
+                <p className="text-lg">Voyager, Photographier, Inspirer</p>
+        </div>
+</div>
+
+    );
+};
+
+export default Wanderly;
